71d96347983df87781babb6f3c3ff13dfb9c71f6
Anushka Sehrawat <asehrawat@tavisca.com>
2018-09-11 19:12:40 +0530

    Cache implementation using decorator pattern in existing project

---
+++ solidapproach/solidapproach/Cache.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solidapproach
+{
+    class Cache:ICache
+    {
+       
+
+        ObjectCache cache = MemoryCache.Default;
+       
+       public void AddtoCache(string key, IProduct value)
+        {
+            if (cache.Get(key) == null)
+            {
+                CacheItemPolicy cacheItemPolicy = new CacheItemPolicy();
+                cacheItemPolicy.AbsoluteExpiration = DateTime.Now.AddHours(1.0);
+                cache.Add(key, value, cacheItemPolicy);
+            }
+
+        }
+       public IProduct getFromCache(string key, IProduct value)
+        {
+            return (IProduct)cache.Get(key);
+        }
+    }
+}
---
+++ solidapproach/solidapproach/ICache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solidapproach
+{
+   public interface ICache
+
+
+    {
+      
+        void AddtoCache(string key, IProduct value);
+        IProduct getFromCache(string key, IProduct value);
+    }
+}
